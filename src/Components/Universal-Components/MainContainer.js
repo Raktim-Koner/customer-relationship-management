259da469c2121733876f392453bd856dd4fd3eef
PayalRoy2004 <payal2004@gmail.com>
2025-07-16 23:56:21 +0530

    sidebar added

---
+++ src/Components/Universal-Components/MainContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+
+const MainContainer = () => {
+  return (
+    <div className='flex'>
+        <Sidebar/>
+      
+    </div>
+  )
+}
+
+export default MainContainer
---
+++ src/Components/Universal-Components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className=' flex items-center h-[41.6rem] w-1/6'
+    style={{ background: "linear-gradient(to bottom, #FDFDFD, #B0E0E6)" }}
+    >
+      
+    </div>
+  )
+}
+
+export default Sidebar
