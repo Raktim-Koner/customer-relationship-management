7b3aced11451a1d18b5aac43a3de8073351b8ba3
rohit-rajak <rohitrjak264@gmail.com>
2025-07-28 16:17:00 +0530

    Fixed heading Colour

--- src/Components/AccountsPage/AccountsName.js
+++ src/Components/AccountsPage/AccountsName.js
@@ -3,7 +3,8 @@ import React from 'react'
 const AccountsName = () => {
   return (
     <div>
-      <h1 className='font-bold text-5xl mt-16 ml-4  font-sans'>ACCOUNTS</h1>
+      {/* Changed the color of the heading */}
+      <h1 className='font-bold text-5xl mt-16 ml-4 text-blue-950 font-sans'>ACCOUNTS</h1>
     </div>
   )
 }
