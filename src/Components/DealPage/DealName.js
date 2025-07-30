816533b5962f95bf2a60aeede9c2729a81a3b867
Payal Roy <payalroy2004@gmail.com>
2025-07-30 22:47:01 +0530

    Added all components in Deal Page

---
+++ src/Components/DealPage/DealName.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DealName = () => {
+  return (
+    <div>
+      {/* chenge the color of the heading */}
+      <h1 className='font-bold text-5xl mt-16 ml-4  font-sans text-blue-950'>DEAL MANAGEMENT</h1> 
+    </div>
+  )
+}
+
+export default DealName
