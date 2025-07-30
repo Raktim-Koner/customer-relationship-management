811fc52e4d2a3c08c7d1b40fcdc50a97289dfd48
rohit-rajak <rohitrjak264@gmail.com>
2025-07-30 18:43:17 +0530

    Linked documentation and Popular topics

--- src/Components/ServicesPage/ServiceTopics.js
+++ src/Components/ServicesPage/ServiceTopics.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import ServiceTopicsLogo from './ServiceTopicsLogo'
+import { Link } from 'react-router-dom'; 
+// Importing the ServiceTopicsLogo component
 const ServiceTopics = () => {
   return (
     <div>
-        <button className="ml-16 mt-24 mr-15 bg-[#E1F7F8] border rounded-md px-7 py-7 flex items-center space-x-2 hover:shadow-md transition duration-200 text-black font-semibold square border-black   ">
+      <Link to="/services/documents" >
+        <button className="ml-16 mt-24 mr-15 bg-[#E1F7F8] border rounded-md px-7 py-7 flex items-center    space-x-2 hover:shadow-md transition duration-200 text-black font-semibold square border-black   ">
           <ServiceTopicsLogo />
           Popular Topics</button>
+      </Link>
     </div>
   )
 }
