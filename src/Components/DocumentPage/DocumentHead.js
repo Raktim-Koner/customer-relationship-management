51f01f3c21aec2b7a2b9f68ed7c799687f6e579b
Raktim-Koner <raktimkoner500@gmail.com>
2025-08-04 22:51:24 +0530

    update the top section of the document page

--- src/Components/DocumentPage/DocumentHead.js
+++ src/Components/DocumentPage/DocumentHead.js
@@ -3,7 +3,7 @@ import React from 'react'
 const DocumentHead = () => {
   return (
     <div>
-        <h1 className='text-5xl text-25414E font-bold font-sans text-blue-950 ml-[4rem] mt-10'>HELP<br/>DOCUMENTATION</h1>
+        <h1 className='text-5xl text-25414E font-bold font-sans text-blue-950 ml-[1.5rem] mt-10'>HELP<br/>DOCUMENTATION</h1>
     </div>
   )
 }
--- src/Components/DocumentPage/DocumentPopularTopics.js
+++ src/Components/DocumentPage/DocumentPopularTopics.js
@@ -1,7 +1,7 @@
 import React from 'react'
 const DocumentPopularTopics = () => {
   return (
-    <div className="ml-[20rem] mt-[2.8rem]">
+    <div className="ml-[22.8rem] mt-[2.8rem]">
       <h1 className='text-5xl text-25414E font-bold font-sans text-blue-950'>POPULAR<br/>TOPICS</h1>
     </div>
   )
