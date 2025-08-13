51c1111e90e4c7c382fa5bb3d6ee87f9b85f9e27
Payal Roy <payalroy2004@gmail.com>
2025-08-13 12:01:30 +0530

    Issues resolved

---
+++ src/Components/Universal-Components/SignSubMainContainer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const SignSubMainContainer = () => {
+  return (
+    <div className='h-[44.1rem] w-[94rem] mx-[1rem] my-[1rem] rounded-lg border border-black' //total width 80=78 + left1 + right1
+     style={{ backgroundColor: '#E1F7F8' }}
+    >
+        <Outlet/>
+      
+    </div>
+  )
+}
+
+export default SignSubMainContainer
