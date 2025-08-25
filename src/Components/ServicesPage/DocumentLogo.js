980dee2b76844084d3f913ffa08307cc2af5d839
rohit-rajak <rohitrjak264@gmail.com>
2025-08-25 11:36:22 +0530

    fixed sidebar and service support page

--- src/Components/ServicesPage/DocumentLogo.js
+++ src/Components/ServicesPage/DocumentLogo.js
@@ -3,7 +3,7 @@ import documentation from '../../Assets/documentation.png.png'
  // Assuming you have a Document.png in Assets
 const DocumentLogo= () => {
     return(
-        <div className="items-center ">
+        <div className="items-center mr-2 ">
             <img src={documentation} alt="Document Logo" className=" items-center h-16 w-14 ml-6" />
             
         </div>
