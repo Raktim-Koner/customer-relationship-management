38f8a9cef309b412d1d144bf26c86508323b0aa5
RAKTIM <raktimkoner500@gmail.com>
2025-09-13 22:00:25 +0530

    chenge

--- src/Components/ProjectPage/Projectfilter.js
+++ src/Components/ProjectPage/Projectfilter.js
@@ -3,7 +3,7 @@ import filter from "../../Assets/filter.png.png";
 
 const Projectfilter = () => {
   return (
-    <div className="items-center mt-1 ml-[35rem]">
+    <div className="items-center mt-1 ml-[34.99rem]">
       <img src={filter} alt="Filter" className="h-8 w-7" />
     </div>
   )
