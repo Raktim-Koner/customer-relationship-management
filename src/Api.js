928d4a521f0b1ca07db40854efaf61de72c22ee4
Raktim Koner <raktimkoner500@gmail.com>
2026-04-30 20:08:54 +0530

    chenge

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,7 @@
 // export const API_URL = "https://api.jsonbin.io/v3/b/68a35031d0ea881f405c99dd";
 // export const API_KEY = "$2a$10$G/HlnQAYpisDw2MDqTuJqefIWbRD3NM39enboXGgbNomTtQZiSmYG";
 
-const BASE_URL = "https://crm-server-dun.vercel.app";
+const BASE_URL = "http://localhost:5000";
 
 /* =================================
    CUSTOMER APIs
